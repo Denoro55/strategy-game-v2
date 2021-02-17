305f0b0dc5515f9eddd3bfb7ad71b4dc03943d26
Denis Chertenko <denis.chertenko@mail.ru>
2021-02-17 23:14:35 +0300

    build(webpack): init project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/scripts/index.ts',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+        title: 'Strategy Game',
+        template: './src/index.html',
+    }),
+    new CleanWebpackPlugin()
+  ],
+};
